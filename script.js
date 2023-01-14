9e7199821e11f71f140ca744005eafc44a9a0f14
moses <mking78911@gmail.com>
2023-01-14 07:31:57 +0000

    get random

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //
-  
+//options of password characters to choose from
+
 // Array of special characters to be included in password
 var specialCharacters = [
   '@',
@@ -95,37 +96,43 @@ function getPasswordOptions() {
   var passwordLength = parseInt(prompt ("Choose a password length between 10 - 64 characters"));
 
   //if these are false then the user would be alerted to enter a number between 10 - 64
-
-  if (isNaN (passwordLength)) || passwordLength < 10 || passwordLength > 64 {
+  while (isNaN (passwordLength) || passwordLength < 10 || passwordLength > 64) {
     alert("Please enter a number between 10-64");
-    return;
-  }
+     passwordLength = parseInt(prompt ("Choose a password length between 10 - 64 characters"));
+    }
 
-  //options of password characters to choose from
-  var choiceArr = []
-
+    var choiceArr= [];
 //if user confirms, special characters would be added to the choiceArr
 
-  if (confirm("Would you like special characters? ($@%&*, etc)"));{
+  if (confirm("Would you like special characters? ($@%&*, etc)")){
     choiceArr = choiceArr.concat(specialCharacters);
   }
 
   if (confirm("Would you like to include numbers?")){
     choiceArr = choiceArr.concat(numericCharacters);
   }
-  var promptLowercase = prompt ("Would you like lowercase?")
-  var promptUppercase = prompt ("would you like uppercase?")
+  if (confirm("Would you like lowercase?")){
+    choiceArr = choiceArr.concat(lowerCasedCharacters);
+
+  }
+  if (confirm("would you like uppercase?")){
+    choiceArr = choiceArr.concat(upperCasedCharacters);
+
+  }
+  return {passwordLength: passwordLength, choiceArr: choiceArr};
 
 }
 
 // Function for getting a random element from an array
 
 function getRandom(arr) {
-
+  return arr[Math.floor(math.random() * arr.length)];
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  var passwordOptions = getPasswordOptions();
+  var passwordLength = passwordOptions.passwordLength
 
 }
 
